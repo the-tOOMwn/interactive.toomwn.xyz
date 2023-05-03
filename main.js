744c2193667497b0910bda9a284050faf9989b4c
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2023-05-03 20:10:39 +0800

    since u guys wanted everything in the html file, so....

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ canvas.style.cursor = 'none';
 var keyboard = {};
 var vy = 0;
 var r = {x:0, y:0}
+var typeing = false;
 window.onkeyup = function(e) { keyboard[e.key.toLowerCase()] = false; }
 window.onkeydown = function(e) { keyboard[e.key.toLowerCase()] = true; }
 
@@ -38,15 +39,16 @@ var createScene = function () {
     });*/
     
     var camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
-    /*
+    var scene = new BABYLON.Scene(engine);
+
+    var camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+
+    // by default all music plays at volume
+    var musicvalue = 1
+
     camera.speed = 0.2
     camera.minZ = 0.01
-    camera.position.y = 0.7
-    
-    const assumedFramesPerSecond = 60;
-    const earthGravity = -9.81;
-    scene.gravity = new BABYLON.Vector3(0, earthGravity / assumedFramesPerSecond, 0);
-    camera.applyGravity = true;*/
+    camera.position.y = 1.8 // double the height of the ellipsoid for some reason
 
     camera.ellipsoid = new BABYLON.Vector3(0.7, 0.7, 0.7);
 
@@ -55,30 +57,54 @@ var createScene = function () {
 
     camera.setTarget(BABYLON.Vector3.Zero());
     
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 100, height: 100}, scene);
+    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 150, height: 150}, scene);
 
     const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-    groundMaterial.emissiveColor = new BABYLON.Color4.FromHexString("#ae854f");
+    groundMaterial.emissiveColor = new BABYLON.Color4(0.8359375, 0.4140625, 0.234375)
     ground.material = groundMaterial;
     ground.checkCollisions = true;
     
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     var light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(10, 50, 10), scene)
+    // const light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1, 50, 1), scene)
     light.diffuse = new BABYLON.Color3(1, 1, 1);
-
-    light.intensity = 0.25;
+    
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 1;
     
     light.setEnabled(true)
 
     light.excludedMeshes.push(ground)
-
-    scene.fogMode = BABYLON.Scene.FOGMODE_EXP
-    scene.fogDensity = 0.01
     
-    BABYLON.SceneLoader.ImportMesh("", "models/", "farm_house.glb", scene, function(newMeshes){
-    newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
-    newMeshes[0].position = new BABYLON.Vector3(2, 4.9, 2)
+    
+    scene.clearColor = new BABYLON.Color3(89/256, 147/256, 255/256) // The colour of the sky
+
+    
+    scene.fogMode = BABYLON.Scene.FOGMODE_EXP
+    scene.fogDensity = 0.02
+        
+    // just adding a box
+    const box = BABYLON.SceneLoader.ImportMesh("", "models/", "soundbox.obj", scene, function(newMeshes){
+    newMeshes[0].scaling = new BABYLON.Vector3(1, 1, 1);
+    newMeshes[0].position = new BABYLON.Vector3(0, 10, 10)
     newMeshes[0].checkCollisions = true;
     });
+
+    const soundbox = BABYLON.SceneLoader.ImportMesh("", "models/", "soundbox.obj", scene, function(newMeshes){
+        newMeshes[0].position = new BABYLON.Vector3(0, 1, 0)
+        newMeshes[0].checkCollisions = true;
+    });
+
+    const music1 = new BABYLON.Sound("Music", "music/verslaflamme.mp3"/*"music/e.wav"*/, scene, null, {
+        loop: true,
+        autoplay: true,
+        volume: 0.4 * musicvalue,
+        spatialSound: true,
+        distanceModel: "exponential",
+        rolloffFactor: 1.1,
+    });
+
+    music1.setPosition(new BABYLON.Vector3(0, 1, 0));
 
     scene.onBeforeRenderObservable.add(() => {
         // Rotate the camera based on the mouse movement
@@ -99,28 +125,28 @@ var createScene = function () {
         
         // basic movement in the horizontal plane
         var moveDirection = BABYLON.Vector3.Zero();
-        if (keyboard['arrowup'] || keyboard['w']) {
+        if ((keyboard['arrowup'] || keyboard['w']) && typeing == false) {
             //console.log('w');
             moveDirection.addInPlace(forward);
         }
-        if (keyboard['arrowdown'] || keyboard['s']) {
+        if ((keyboard['arrowdown'] || keyboard['s']) && typeing == false) {
             //console.log('s');
             moveDirection.subtractInPlace(forward);
         }
-        if (keyboard['arrowright'] || keyboard['d']) {
+        if ((keyboard['arrowright'] || keyboard['d']) && typeing == false) {
             //console.log('d');
             moveDirection.addInPlace(right);
         }
-        if (keyboard['arrowleft'] || keyboard['a']) {
+        if ((keyboard['arrowleft'] || keyboard['a']) && typeing == false) {
             //console.log('a');
             moveDirection.subtractInPlace(right);
         }
 
         // Jumping
-        if (keyboard[' '] && camera.position.y < 1.2) {
+        if ((keyboard[' '] && camera.position.y < 1.2) && typeing == false) {
             vy += 2; // change this to change the jump power
         }
-        //console.log(keyboard);
+        console.log(keyboard);
         //console.log(camera.position);
         if (camera.position.y > 1) { 
             vy -= 0.4;
@@ -134,7 +160,7 @@ var createScene = function () {
 
         // Sneaking
         var shift = new BABYLON.Vector3(0, -0.5, 0);
-        if (keyboard['shift']) {
+        if (keyboard['shift'] && typeing == false) {
             moveDirection.addInPlace(shift);
         }
 
@@ -168,3 +194,4 @@ initFunction().then(() => {sceneToRender = scene });
 window.addEventListener("resize", function () {
     engine.resize();
 });
+
